c3b59c94372268a07bfdf229c6626b19cc2b9f97
John Janecek <janecektyler@gmail.com>
2022-09-05 22:08:45 -0600

    updates

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,10 @@
 FROM mcr.microsoft.com/dotnet/core/sdk:3.1 as builder
 
-ARG MIDDLEWARE_SERVER
-
 RUN apt-get update
 RUN apt-get install curl gnupg software-properties-common python3 python3-pip -y
-RUN curl https://packages.microsoft.com/keys/microsoft.asc > gpg_key.txt && apt-key add gpg_key.txt 
+RUN curl https://packages.microsoft.com/keys/microsoft.asc > gpg_key.txt && apt-key add gpg_key.txt
 RUN curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | tee /etc/apt/sources.list.d/msprod.list
-RUN apt-get update 
+RUN apt-get update
 ENV ACCEPT_EULA=Y
 RUN apt-get install mssql-tools unixodbc-dev -y
 RUN echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
--- configuredb.py
+++ configuredb.py
@@ -4,6 +4,7 @@ import pyodbc
 import pandas as pd
 import time
 import subprocess
+import json
 
 USER = os.getenv('DB_USER')
 PASSWORD = os.getenv('DB_PASSWORD')
@@ -11,26 +12,26 @@ SERVER = os.getenv('DB_SERVER')
 MIDDLEWARE_USER = os.getenv('MIDDLEWARE_USER')
 MIDDLEWARE_PASSWORD = os.getenv('MIDDLEWARE_PASSWORD')
 MIDDLEWARE_SERVER_IP = os.getenv('MIDDLEWARE_SERVER_IP')
-APP_ID = os.getenv("APP_ID")
-APP_NAME = os.getenv("APP_NAME")
 
 print(f"USER: {USER}")
-print(f"PASSWORD: {PASSWORD}")
+#print(f"PASSWORD: {PASSWORD}")
 print(f"SERVER: {SERVER}")
 print(f"MIDDLEWARE_USER: {MIDDLEWARE_USER}")
-print(f"APP_ID: {APP_ID}")
-print(f"APP_NAME: {APP_NAME}")
 
-#--------------- Start the middleware
+#===============================================
+# Start middleware process
+#===============================================
 print("Starting middleware process ...")
 process = subprocess.Popen(['dotnet', 'Horizon.Database.dll'])
 time.sleep(3)
 
+#===============================================
+# Create admin middleware user
+#===============================================
 print(f"Creating admin user ...")
 curl_command = f'curl --insecure -X POST "{MIDDLEWARE_SERVER_IP}/Account/createAccount" -H  "accept: */*" -H  "Content-Type: application/json-patch+json" -d "{{\\"AccountName\\":\\"{MIDDLEWARE_USER}\\",\\"AccountPassword\\":\\"{MIDDLEWARE_PASSWORD}\\",\\"MachineId\\":\\"1\\",\\"MediusStats\\":\\"1\\",\\"AppId\\":0,\\"PasswordPreHashed\\":false}}"'
 print(curl_command)
 os.system(curl_command)
-
 
 print("Adding role and app id ...")
 cnxn_str = f"DRIVER={{ODBC Driver 17 for SQL Server}};Server={SERVER};Database=Medius_Database;UID={USER};PWD={PASSWORD};"
@@ -51,11 +52,66 @@ print(type(role_id))
 if pd.read_sql(f"select count(*) from accounts.user_role where account_id = {account_id} and role_id = {role_id}", cnxn).values[0][0] == 0:
     cursor.execute(f"INSERT INTO accounts.user_role VALUES({account_id}, {role_id}, GETDATE(), GETDATE(), null)")
 
+#===============================================
+# Update Everything from the config
+#===============================================
+with open('/code/docker_config.json', 'r') as f:
+    config = json.loads(f.read())
 
 #### Insert APP ID into dim table
-if pd.read_sql(f"select count(*) from keys.dim_app_ids where app_id = {APP_ID}", cnxn).values[0][0] == 0:
-    cursor.execute(f"INSERT INTO keys.dim_app_ids VALUES({APP_ID}, '{APP_NAME}', 1)")
+# Multiple groups not supported
+print("Inserting app ids ...")
+for app in config['apps']:
+    APP_ID = app['id']
+    APP_NAME = app['name']
+    APP_GROUP = app['app_group']
+    # If it already exists, ignore
+    if pd.read_sql(f"select count(*) from keys.dim_app_ids where app_id = {APP_ID}", cnxn).values[0][0] == 0:
+        cursor.execute(f"INSERT INTO keys.dim_app_ids VALUES({APP_ID}, '{APP_NAME}', {APP_GROUP})")
 
+#### Insert Channels
+print("Inserting channels ...")
+for channel in config['channels']:
+    id = channel['id']
+    app_id = channel['app_id']
+    name = channel['name']
+    max_players = channel['max_players']
+    generic_field_1 = channel['generic_field_1']
+    generic_field_2 = channel['generic_field_2']
+    generic_field_3 = channel['generic_field_3']
+    generic_field_4 = channel['generic_field_4']
+    generic_field_filter = channel['generic_field_filter']
+
+    if pd.read_sql(f"select count(*) from world.channels where app_id = {app_id} and name = '{name}'", cnxn).values[0][0] == 0:
+        cursor.execute(f"""
+            INSERT INTO world.channels VALUES(
+                {id},
+                {app_id},
+                '{name}',
+                {max_players},
+                {generic_field_1},
+                {generic_field_2},
+                {generic_field_3},
+                {generic_field_4},
+                {generic_field_filter}
+            )
+        """)
+
+#### Insert Locations
+print("Inserting locations ...")
+for location in config['locations']:
+    id = location['id']
+    app_id = location['app_id']
+    name = location['name']
+    # If location exists, ignore
+    if pd.read_sql(f"select count(*) from world.locations where app_id = {app_id} and name = '{name}'", cnxn).values[0][0] == 0:
+        cursor.execute(f"""
+            INSERT INTO world.locations VALUES(
+                {id},
+                {app_id},
+                '{name}'
+            )
+        """)
 
 cnxn.commit()
 
