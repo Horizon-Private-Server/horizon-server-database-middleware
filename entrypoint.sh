ee1350fd96119de792e08dd1ccc20fe62df635f2
John Janecek <janecektyler@gmail.com>
2022-09-20 20:00:59 -0600

    add run.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #! /bin/sh
-echo "Waiting for other containers to start ..."
-sleep 10
 
 # Running create scripts
 echo "Running create scripts ..."
@@ -8,6 +6,7 @@ echo "Running create scripts ..."
 /opt/mssql-tools/bin/sqlcmd -S ${DB_SERVER} -U ${DB_USER} -P "${DB_PASSWORD}" -i ../scripts/CREATE_TABLES.sql
 
 sed -i "s|https://localhost:10000|${MIDDLEWARE_SERVER}|g" /code/out/appsettings.json
+#sed -i "s|Information|Warning|g" /code/out/appsettings.json
 
 echo "Running configuredb.py ..."
 python3 /code/configuredb.py
